48595ebbff9db2e0f454650ee50433b3bd8decc3
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-09-30 12:58:00 -0700

    Layout of storyboard created. IBOutlets for view controller.

---
+++ Bouyant/Bouyant/VolumeCalculatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VolumeCalculatorViewController.swift
+//  Bouyant
+//
+//  Created by Lambda_School_Loaner_204 on 9/30/20.
+//
+
+import UIKit
+
+class VolumeCalculatorViewController: UIViewController {
+
+    // MARK: IBOutlets
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var levelPicker: UIPickerView!
+    @IBOutlet weak var litersLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
